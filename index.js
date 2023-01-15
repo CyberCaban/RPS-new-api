8e14ae7d1f8557898fdd626b90106c4522ffc9ab
CyberCaban <andrewgamer63@gmail.com>
2023-01-15 20:46:50 +0300

    messages+base game

--- index.js
+++ index.js
@@ -22,33 +22,56 @@ app.get('/', (req,res)=>{
 app.use(cors())
 
 let room;
+let compareArray = []
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
 
     //присоединяем юзера к комнате
     socket.on('join room', (roomID)=>{
         room = roomID
         socket.join(roomID);
         
-        let elementArray = []
-        socket.on('player_current_element', (current_element)=>{
-            elementArray.push(current_element)
-            if (elementArray.length == 2) {
-                let player1_elem = elementArray[0]
-                let player2_elem = elementArray[1]
-                
-                if (player1_elem == player2_elem) {
-                    io.in(room).emit(send)
+        socket.on('player_current_element', (PlayerMove)=>{
+            compareArray.push(PlayerMove)
+            console.log(compareArray);
+            if (compareArray.length == 2) {
+                let player1_elem = compareArray[0]
+                let player2_elem = compareArray[1]
+
+                //если НИЧЬЯ
+                if (player1_elem.chosenElement == player2_elem.chosenElement) {
+                    io.in(room).emit('send result', 'DRAW!')
+                    compareArray = []
                 }
-                
+
+                //если побеждает ПЕРВЫЙ
+                if ((player1_elem.chosenElement == 'rock' && player2_elem.chosenElement == 'scissors') || 
+                (player1_elem.chosenElement == 'paper' && player2_elem.chosenElement == 'rock') ||
+                (player1_elem.chosenElement == 'scissors' && player2_elem.chosenElement == 'paper')) {
+
+                    //отправка результатов каждому игрроку
+                    io.to(player1_elem.playerID).emit('send result', 'WIN!')
+                    io.to(player2_elem.playerID).emit('send result', 'MISS!')
+                    compareArray = []
+                }
+
+                //если побеждает ВТОРОЙ
+                if ((player2_elem.chosenElement == 'rock' && player1_elem.chosenElement == 'scissors') || 
+                (player2_elem.chosenElement == 'paper' && player1_elem.chosenElement == 'rock') ||
+                (player2_elem.chosenElement == 'scissors' && player1_elem.chosenElement == 'paper')) {
+                    
+                    //отправка результатов каждому игрроку
+                    io.to(player2_elem.playerID).emit('send result', 'WIN!')
+                    io.to(player1_elem.playerID).emit('send result', 'MISS!')
+                    compareArray = []
+                }     
             }
         })
     })
 
     //получение сообщения сервером и отправка их юзеру
     socket.on('send message', (message)=>{
-        io.emit('getMessage', message)
+        io.to(room).emit('getMessage', message)
     })
 });
 
